d0f7536cebe3e7208d60b909b0a12e7fcd492f13
vinayms96 <vinay.srikrishna@gmail.com>
2019-03-12 23:23:38 +0530

    amazon del

--- Amazon/src/generic/Auto_constant.java
+++
@@ -1,8 +0,0 @@
-package generic;
-
-public interface Auto_constant {
-	String key = "webdriver.chrome.driver";
-	String value="./Software/chromedriver.exe";
-	String excelpath="./data/excel.xlsx";
-	String photopath="./Picture";
-}
